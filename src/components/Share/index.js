25c45c2d267d800e61d4207332b48cab5bc3dc03
chaiwattsw <tongbboy29@hotmail.com>
2022-04-18 14:42:03 +0700

    seperate share component

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -1,6 +1,43 @@
 import React from "react";
 
 const Share = () => {
+  // const shareComponent = useRef();
+
+  // const downloadImage = async () => {
+  //   const element = shareComponent.current;
+  //   const canvas = await html2canvas(element, {
+  //     allowTaint: true,
+  //     useCORS: true,
+  //   });
+
+  //   const data = canvas.toDataURL("image/png");
+  //   const link = document.createElement("a");
+
+  //   if (typeof link.download === "string") {
+  //     link.href = data;
+  //     link.download = "hype.png";
+
+  //     document.body.appendChild(link);
+  //     link.click();
+  //     document.body.removeChild(link);
+  //   } else {
+  //     window.open(data);
+  //   }
+  // };
+
+  /* <button onClick={downloadImage} className="bg-green-500 p-6">
+        Share
+      </button>
+
+      <div ref={shareComponent} className="grid grid-cols-5 gap-6">
+        {data?.items?.map((item) => (
+          <div>
+            <img src={item.album.images[1].url} />
+            <p className="text-black">{item.name}</p>
+          </div>
+        ))}
+      </div> */
+
   return <div>Share</div>;
 };
 
--- src/components/TopTracks/index.js
+++ src/components/TopTracks/index.js
@@ -1,5 +1,4 @@
-import React, { useState, useRef } from "react";
-import html2canvas from "html2canvas";
+import React, { useState } from "react";
 import { useSpotify } from "../../hooks/useSpotify";
 import TopItemsContainer from "../TopItemsContainer";
 import TopItemsSkeleton from "../TopItemsSkeleton";
@@ -10,29 +9,6 @@ const TopTracks = () => {
   const { data } = useSpotify(
     `https://api.spotify.com/v1/me/top/tracks?time_range=${duration}&limit=10`
   );
-  // const shareComponent = useRef();
-
-  // const downloadImage = async () => {
-  //   const element = shareComponent.current;
-  //   const canvas = await html2canvas(element, {
-  //     allowTaint: true,
-  //     useCORS: true,
-  //   });
-
-  //   const data = canvas.toDataURL("image/png");
-  //   const link = document.createElement("a");
-
-  //   if (typeof link.download === "string") {
-  //     link.href = data;
-  //     link.download = "hype.png";
-
-  //     document.body.appendChild(link);
-  //     link.click();
-  //     document.body.removeChild(link);
-  //   } else {
-  //     window.open(data);
-  //   }
-  // };
 
   return (
     <TopItemsContainer
@@ -40,19 +16,6 @@ const TopTracks = () => {
       duration={duration}
       setDuration={setDuration}
     >
-      {/* <button onClick={downloadImage} className="bg-green-500 p-6">
-        Share
-      </button>
-
-      <div ref={shareComponent} className="grid grid-cols-5 gap-6">
-        {data?.items?.map((item) => (
-          <div>
-            <img src={item.album.images[1].url} />
-            <p className="text-black">{item.name}</p>
-          </div>
-        ))}
-      </div> */}
-
       {data ? <TopTrackItems data={data} /> : <TopItemsSkeleton />}
     </TopItemsContainer>
   );
