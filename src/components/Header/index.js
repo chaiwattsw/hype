033b3ab0511f0d56aeddd3af676f36ed1e6f6a3a
chaiwattsw <tongbboy29@hotmail.com>
2022-04-19 13:02:29 +0700

    add text shadow to logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <nav className="bg-green-500 border-none flex items-center flex-row justify-between sticky top-0 z-50 w-full p-6 text-white font-bold">
-      <h3 className="text-3xl font-bold">HYPE.</h3>
+      <h3 className="text-3xl font-bold [text-shadow:-2.5px_2.5px_0_#000]">
+        HYPE.
+      </h3>
       <div className="flex-row justify-center gap-16 hidden md:flex text-lg">
         <Link to="/">Top Tracks</Link>
         <Link to="recommendations">Recommendations</Link>
