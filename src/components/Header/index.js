0336ee9be550f51ac4b35cb3ee63519128813ffd
chaiwattsw <tongbboy29@hotmail.com>
2022-04-19 13:20:38 +0700

    change header text

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,11 +4,14 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <nav className="bg-green-500 border-none flex items-center flex-row justify-between sticky top-0 z-50 w-full p-6 text-white font-bold">
-      <h3 className="text-3xl font-bold [text-shadow:-2.5px_2.5px_0_#000]">
+      <Link
+        to="/"
+        className="text-3xl font-bold [text-shadow:-2.5px_2.5px_0_#000]"
+      >
         HYPE.
-      </h3>
+      </Link>
       <div className="flex-row justify-center gap-16 hidden md:flex text-lg">
-        <Link to="/">Top Tracks</Link>
+        <Link to="/">Home</Link>
         <Link to="recommendations">Recommendations</Link>
         {/* <Link to="/">Palette</Link> */}
       </div>
