62932455fa9a01e0e900aabdf540c54e73497e74
chaiwattsw <tongbboy29@hotmail.com>
2022-04-21 14:39:21 +0700

    change spotify  scope

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,18 +1,18 @@
 const redirectURI = "http://localhost:3000";
 const clientID = "db3a0997982646619d04c0837edfe658";
 const scopes = [
-  "streaming",
   "user-modify-playback-state",
   "user-read-private",
-  "user-read-email",
-  "user-read-playback-state",
   "user-top-read",
   "user-library-modify",
   "user-library-read",
+  "user-read-recently-played",
 ];
 export const authEndPoint = "https://accounts.spotify.com/authorize";
 
-export const loginURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=code&redirect_uri=${redirectURI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read`;
+export const loginURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=code&redirect_uri=${redirectURI}&scope=${scopes.join(
+  "%20"
+)}`;
 
 export const durationString = {
   short_term: "This month",
