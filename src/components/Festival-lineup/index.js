36be37846898f18b9a1fd335032671bd3d36e9fe
chaiwattsw <tongbboy29@hotmail.com>
2022-04-29 03:08:37 +0700

    seperate festival component

--- src/components/Festival-lineup/index.js
+++ src/components/Festival-lineup/index.js
@@ -1,10 +1,10 @@
-import React, { useRef } from "react";
+import React from "react";
 import { useSpotify } from "../../hooks/useSpotify";
-import generateImage from "../../utils/generateImage";
-import { getTodayDate } from "../../utils/getDate";
+
+import FestivalPoster from "./festival-poster";
+import FestivalSkeleton from "./festival-skeleton";
 
 const FestivalLineup = () => {
-  const componentRef = useRef();
   const getArtists = {
     method: "get",
     url: `https://api.spotify.com/v1/me/top/artists?time_range=long_term&limit=50`,
@@ -13,95 +13,7 @@ const FestivalLineup = () => {
 
   return (
     <div className="relative w-full flex flex-wrap flex-col my-8 text-white justify-center items-center text-center">
-      <button
-        onClick={() =>
-          generateImage(componentRef, "festival-lineup", "hype-festival-lineup")
-        }
-        className="bg-gray-900 hover:bg-gray-800 px-4 py-1 rounded-2xl w-20 font-semibold mb-4"
-      >
-        Share
-      </button>
-      <div
-        ref={componentRef}
-        id="festival-lineup"
-        className="h-[750px] w-full md:h-[901px] md:w-[660px] bg-[url('../src/assets/images/festival-lineup/bg.jpg')] bg-no-repeat bg-center bg-cover"
-      >
-        <div className="w-full py-6 px-8 md:px-12 flex flex-col justify-center items-center">
-          <div className="mb-2">
-            <h2 className="text-5xl font-bold [text-shadow:-5px_5px_0_#000]">
-              HYPE
-            </h2>
-            <p className="text-2xl font-bold [text-shadow:-5px_5px_0_#000]">
-              MUSIC FESTIVAL
-            </p>
-          </div>
-          <div className="w-11/12 mt-4">
-            <div className="relative flex items-center">
-              <div className="flex-grow border-t border-2 border-white"></div>
-              <span
-                id="festival-date"
-                className="flex-shrink mx-4 text-lg font-semibold"
-              >
-                {getTodayDate()}
-              </span>
-              <div className="flex-grow border-t border-2 border-white"></div>
-            </div>
-          </div>
-          <div id="festival-artists">
-            {data?.items?.map((artist, artistIdx) => {
-              if (artistIdx <= 2) {
-                return (
-                  <span
-                    name="festival-artists-head"
-                    key={artist.id}
-                    className="text-2xl md:text-6xl font-bold"
-                  >
-                    {artist.name} &bull;{" "}
-                  </span>
-                );
-              }
-              if (artistIdx <= 20) {
-                return (
-                  <span
-                    name="festival-artists-second"
-                    key={artist.id}
-                    className="text-xl md:text-4xl font-semibold"
-                  >
-                    {artist.name} &bull;{" "}
-                  </span>
-                );
-              }
-              if (artistIdx >= 21 && artistIdx <= 48) {
-                return (
-                  <span
-                    name="festival-artists-third"
-                    key={artist.id}
-                    className="text-md md:text-2xl"
-                  >
-                    {artist.name} &bull;{" "}
-                  </span>
-                );
-              }
-              return (
-                <span
-                  name="festival-artists-third"
-                  key={artist.id}
-                  className="text-md md:text-2xl"
-                >
-                  {artist.name}
-                </span>
-              );
-            })}
-          </div>
-
-          <div className="absolute bottom-8">
-            <p className="text-lg font-medium">Get your own music festival</p>
-            <h4 className="text-3xl font-bold [text-shadow:-5px_5px_0_#000]">
-              HYPE
-            </h4>
-          </div>
-        </div>
-      </div>
+      {data ? <FestivalPoster data={data} /> : <FestivalSkeleton />}
     </div>
   );
 };
