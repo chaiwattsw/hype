1030c71de330047f5e00f26a483be3162edb15c9
chaiwattsw <tongbboy29@hotmail.com>
2022-07-09 03:35:51 +0700

    refactor fetcher

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,1 +1,16 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+const client = axios.create({ baseURL: "https://api.spotify.com/v1" });
+
+const fetcher = async (config: Record<string, unknown>) => {
+  const token = window.localStorage.getItem("hype_client_token");
+  client.defaults.headers.common.Authorization = `Bearer ${token}`;
+  const onSuccess = (response: AxiosResponse) => response.data;
+  const onError = (error: AxiosError) => {
+    throw new Error(error.message);
+  };
+
+  return client(config).then(onSuccess).catch(onError);
+};
+
+export default fetcher;
