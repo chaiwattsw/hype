019f760fa7e769ded0c433b6f4e2a2ac6b1c3591
Chaiwat Trisuwan <tongbboy29@hotmail.com>
2022-07-12 15:10:59 +0700

    add text when recommended feature is not available

--- src/components/Recommended/index.tsx
+++ src/components/Recommended/index.tsx
@@ -4,7 +4,21 @@ import Seeds from "./Seeds";
 import RecommendedList from "./RecommendedList";
 
 const Recommended = () => {
-  const { data: recommendation } = useRecommendation();
+  const { data: recommendation, isError } = useRecommendation();
+
+  if (isError) {
+    return (
+      <div className="text-center flex items-center justify-center flex-col min-h-screen">
+        <p className="text-9xl">😥</p>
+        <div className="mt-12">
+          <p className="text-4xl font-bold text-white">Sorry!</p>
+          <p className="text-xl font-bold text-white mt-4">
+            This feature only available for whitelisted user
+          </p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div>
