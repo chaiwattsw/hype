1e36e3b7b798e637fa14a5c2360b9b28680649cd
Chaiwat Trisuwan <tongbboy29@hotmail.com>
2022-07-12 15:56:41 +0700

    try to fix localstorage problem in mobile browser

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,17 +1,16 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 
-const client = axios.create({
-  baseURL: "https://api.spotify.com/v1",
-});
-client.interceptors.request.use((config) => {
-  const token = localStorage.getItem("hype_access_token");
-  if (config.headers) {
-    config.headers.Authorization = token ? `Bearer ${token}` : "";
-  }
-  return config;
-});
-
 async function fetcher(config: Record<string, unknown>) {
+  const client = axios.create({
+    baseURL: "https://api.spotify.com/v1",
+  });
+  client.interceptors.request.use((config) => {
+    const token = localStorage.getItem("hype_access_token");
+    if (config.headers) {
+      config.headers.Authorization = token ? `Bearer ${token}` : "";
+    }
+    return config;
+  });
   const onSuccess = (response: AxiosResponse) => response.data;
   const onError = (error: AxiosError) => {
     throw new Error(error.message);
